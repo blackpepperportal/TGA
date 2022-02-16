0bacb4d25fc07c79b4c7e555b5e3e731df997b41
blackpepperportal <sb@revolv.vip>
2022-02-16 17:57:33 +0400

    v2

---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+const tsParticles = require("tsparticles");
+
+tsParticles
+  .loadJSON("tsparticles", "presets/default.json")
+  .then((container) => {
+    console.log("callback - tsparticles config loaded");
+  })
+  .catch((error) => {
+    console.error(error);
+  });
